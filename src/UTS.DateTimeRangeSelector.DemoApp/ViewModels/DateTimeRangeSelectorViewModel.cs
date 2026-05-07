7d1bd9a5d80e792285d9195ba022ffe3b4393005
Dankovvi-Work <vidankov@yandex.ru>
2026-05-07 20:03:46 +0300

    Complete demo for DateTimeRangeSelector with toggleable Orientation
    - Replace stub ViewModel with actual RangeFrom/RangeTo and orientation state
    - Add ToggleOrientation command
    - Bind control properties to ViewModel in the view, show current values

--- src/UTS.DateTimeRangeSelector.DemoApp/ViewModels/DateTimeRangeSelectorViewModel.cs
+++ src/UTS.DateTimeRangeSelector.DemoApp/ViewModels/DateTimeRangeSelectorViewModel.cs
@@ -1,4 +1,5 @@
-﻿using ReactiveUI;
+﻿using Avalonia.Layout;
+using ReactiveUI;
 using ReactiveUI.SourceGenerators;
 
 namespace UTS.DateTimeRangeSelector.DemoApp.ViewModels;
@@ -9,5 +10,11 @@ public partial class DateTimeRangeSelectorViewModel(IScreen hostScreen) : Reacti
     public IScreen HostScreen { get; } = hostScreen;
     public ViewModelActivator Activator { get; } = new();
 
-    [Reactive] private string _message = "Work in progress";
+    [Reactive] private DateTime? _rangeFrom = DateTime.UtcNow.AddDays(-1);
+    [Reactive] private DateTime? _rangeTo = DateTime.UtcNow.AddDays(1).AddHours(23).AddMinutes(59).AddSeconds(59).AddMilliseconds(999);
+    [Reactive] private Orientation _orientation = Orientation.Vertical;
+
+    [ReactiveCommand] private void ToggleOrientation() =>
+        Orientation = Orientation == Orientation.Vertical
+            ? Orientation.Horizontal : Orientation.Vertical;
 }
