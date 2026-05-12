596697e487e3b48c87e07c4c628038392136a76b
Dankovvi-Work <vidankov@yandex.ru>
2026-05-12 18:38:12 +0300

    Add preset demonstration to DateTimeRangeSelector view
    - Add Presets, ShowPresets, ToggleShowPresets to DateTimeRangeSelectorViewModel
    - Bind Presets and ShowPresets in DateTimeRangeSelectorView

--- src/UTS.DateTimeRangeSelector.DemoApp/ViewModels/DateTimeRangeSelectorViewModel.cs
+++ src/UTS.DateTimeRangeSelector.DemoApp/ViewModels/DateTimeRangeSelectorViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Avalonia.Layout;
 using ReactiveUI;
 using ReactiveUI.SourceGenerators;
+using UTS.DateTimeRangeSelector.Core;
 
 namespace UTS.DateTimeRangeSelector.DemoApp.ViewModels;
 
@@ -9,12 +10,19 @@ public partial class DateTimeRangeSelectorViewModel(IScreen hostScreen) : Reacti
     public string? UrlPathSegment => "DateTimeRangeSelector";
     public IScreen HostScreen { get; } = hostScreen;
     public ViewModelActivator Activator { get; } = new();
+    public List<PresetItem> Presets =>
+    [
+        new ("Последний час", TimeSpan.FromHours(1)),
+        new ("Последние сутки", TimeSpan.FromDays(1)),
+        new ("За весь период", RangeMax!.Value - RangeMin!.Value)
+    ];
 
     [Reactive] private DateTime? _rangeFrom = DateTime.UtcNow.AddDays(-1);
     [Reactive] private DateTime? _rangeTo = DateTime.UtcNow.AddDays(1).AddHours(23).AddMinutes(59).AddSeconds(59).AddMilliseconds(999);
     [Reactive] private DateTime? _rangeMin = DateTime.UtcNow.Date.AddDays(-7);
     [Reactive] private DateTime? _rangeMax = DateTime.UtcNow.Date.AddDays(7).AddHours(23).AddMinutes(59).AddSeconds(59).AddMilliseconds(999);
     [Reactive] private Orientation _orientation = Orientation.Vertical;
+    [Reactive] private bool _showPresets = false;
 
     [ReactiveCommand] private void ToggleOrientation() =>
         Orientation = Orientation == Orientation.Vertical
@@ -23,4 +31,5 @@ public partial class DateTimeRangeSelectorViewModel(IScreen hostScreen) : Reacti
     [ReactiveCommand] private void DecreaseMin() => RangeMin = RangeMin?.AddDays(-1);
     [ReactiveCommand] private void IncreaseMax() => RangeMax = RangeMax?.AddDays(1);
     [ReactiveCommand] private void DecreaseMax() => RangeMax = RangeMax?.AddDays(-1);
+    [ReactiveCommand] private void ToggleShowPresets() => ShowPresets = !ShowPresets;
 }
